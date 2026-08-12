6983dca8e7f8ad4d1b778df6034ffbb86ce1ce91
JokubasKul <jokubas.kulbokas@gmail.com>
2026-08-12 20:05:36 +0300

    0.1.0 "task class, tasks json, basic json reading"

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Tasks {
+
+    private int id;
+    private String task;
+    private String colour;
+    private boolean isComplete;
+
+    public Tasks() {}
+
+    public Tasks(int id, String task, String colour, boolean isComplete){
+        this.id=id;
+        this.task=task;
+        this.colour=colour;
+        this.isComplete=isComplete;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public boolean getIsComplete() {
+        return isComplete;
+    }
+    public void setIsComplete(boolean complete) {
+        this.isComplete = complete;
+    }
+}
